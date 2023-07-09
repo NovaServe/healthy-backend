bce93e4527a3e346b68bc3f62ec832f749d1c901
Oleg Kuchrenko <OlegKDeveloper@gmail.com>
2023-07-09 15:09:36 +0300

    Initial commit (#1)

---
+++ src/main/java/healthy/lifestyle/assistant/server/HelloWorldController.java
@@ -0,0 +1,17 @@
+package healthy.lifestyle.assistant.server;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+    @GetMapping("/")
+    public ResponseEntity<Dto> helloWorld() {
+        return ResponseEntity.ok(new Dto("Hello World"));
+    }
+}
+
+record Dto(String message) {}
