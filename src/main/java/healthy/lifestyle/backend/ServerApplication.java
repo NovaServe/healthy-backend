d0c08117df0366318c60a3affc0613e22c183b7d
OlegKDev <OlegKDeveloper@gmail.com>
2023-07-15 15:43:40 +0300

    Rename directories

--- src/main/java/healthy/lifestyle/backend/ServerApplication.java
+++ src/main/java/healthy/lifestyle/backend/ServerApplication.java
@@ -1,4 +1,4 @@
-package healthy.lifestyle.assistant.server;
+package healthy.lifestyle.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/healthy/lifestyle/backend/ServerApplicationTests.java
+++ src/test/java/healthy/lifestyle/backend/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package healthy.lifestyle.assistant.server;
+package healthy.lifestyle.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
