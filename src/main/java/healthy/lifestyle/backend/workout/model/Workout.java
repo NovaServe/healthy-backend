ce83024852f9ca748bd9ce25a8d5ef15be562b4f
OlegKDev <OlegKDeveloper@gmail.com>
2023-10-17 18:42:36 +0300

    Get Default Workouts #74

---
+++ src/main/java/healthy/lifestyle/backend/workout/model/Workout.java
@@ -0,0 +1,34 @@
+package healthy.lifestyle.backend.workout.model;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import lombok.*;
+
+@Entity
+@Table(name = "workouts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", unique = false, nullable = false)
+    private String title;
+
+    @Column(name = "description", unique = false, nullable = true)
+    private String description;
+
+    @Column(name = "is_custom", unique = false, nullable = false)
+    private boolean isCustom;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "workouts_exercises",
+            joinColumns = @JoinColumn(name = "workout_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "exercise_id", referencedColumnName = "id"))
+    private Set<Exercise> exercises;
+}
