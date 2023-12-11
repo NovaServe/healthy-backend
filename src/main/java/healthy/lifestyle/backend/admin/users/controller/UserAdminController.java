175bccd5eb292dc68c81627d363da1c230cd7ef1
Anastasiia Shevchuk <shevchuk2834@gmail.com>
2023-12-11 17:11:30 +0200

    Get Users (Admin) Service+Controller #153

--- src/main/java/healthy/lifestyle/backend/admin/users/controller/UserAdminController.java
+++ src/main/java/healthy/lifestyle/backend/admin/users/controller/UserAdminController.java
@@ -1,33 +1,37 @@
 package healthy.lifestyle.backend.admin.users.controller;
 
-import healthy.lifestyle.backend.admin.users.service.AdminService;
+import healthy.lifestyle.backend.admin.users.service.UserAdminService;
 import healthy.lifestyle.backend.users.dto.UserResponseDto;
 import healthy.lifestyle.backend.users.model.Country;
+import healthy.lifestyle.backend.users.model.Role;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("${api.basePath}/${api.version}/admin")
-public class AdminController {
-    private final AdminService adminService;
+public class UserAdminController {
+    private final UserAdminService adminService;
 
-    public AdminController(AdminService adminService) {
+    public UserAdminController(UserAdminService adminService) {
         this.adminService = adminService;
     }
 
     @GetMapping("/users")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<UserResponseDto>> getAllUsers(
+            @RequestParam(name = "role", required = true) Role role,
             @RequestParam(name = "username", required = false) String username,
             @RequestParam(name = "email", required = false) String email,
             @RequestParam(name = "fullName", required = false) String fullName,
             @RequestParam(name = "country", required = false) Country country,
             @RequestParam(name = "age", required = false) Integer age) {
-        return ResponseEntity.ok(adminService.getUsersByFilters(username, email, fullName, country, age));
+        return ResponseEntity.ok(adminService.getUsersByFilters(role, username, email, fullName, country, age));
     }
 }
--- src/main/java/healthy/lifestyle/backend/admin/users/service/UserAdminServiceImpl.java
+++ src/main/java/healthy/lifestyle/backend/admin/users/service/UserAdminServiceImpl.java
@@ -1,13 +1,12 @@
 package healthy.lifestyle.backend.admin.users.service;
 
-import static java.util.Objects.isNull;
-
+import healthy.lifestyle.backend.admin.users.repository.UserAdminRepository;
 import healthy.lifestyle.backend.exception.ApiException;
 import healthy.lifestyle.backend.exception.ErrorMessage;
 import healthy.lifestyle.backend.users.dto.UserResponseDto;
 import healthy.lifestyle.backend.users.model.Country;
+import healthy.lifestyle.backend.users.model.Role;
 import healthy.lifestyle.backend.users.model.User;
-import healthy.lifestyle.backend.users.repository.UserRepository;
 import java.util.Comparator;
 import java.util.List;
 import org.modelmapper.ModelMapper;
@@ -15,23 +14,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdminServiceImpl implements AdminService {
+public class UserAdminServiceImpl implements UserAdminService {
 
-    private final UserRepository userRepository;
+    private final UserAdminRepository userAdminRepository;
 
     private final ModelMapper modelMapper;
 
-    public AdminServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
-        this.userRepository = userRepository;
+    public UserAdminServiceImpl(UserAdminRepository userAdminRepository, ModelMapper modelMapper) {
+        this.userAdminRepository = userAdminRepository;
         this.modelMapper = modelMapper;
     }
 
     @Override
     public List<UserResponseDto> getUsersByFilters(
-            String username, String email, String fullName, Country country, Integer age) {
-        List<User> users = userRepository.findByFilters(username, email, fullName, country, age);
-        if (isNull(users) || users.size() == 0)
-            throw new ApiException(ErrorMessage.SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+            Role role, String username, String email, String fullName, Country country, Integer age) {
+        List<User> users = userAdminRepository
+                .findByFilters(role, username, email, fullName, country, age)
+                .orElseThrow(() -> new ApiException(ErrorMessage.NOT_FOUND, HttpStatus.NOT_FOUND));
 
         return users.stream()
                 .map(user -> modelMapper.map(user, UserResponseDto.class))
